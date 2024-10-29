c30578a12cd6e0f2539de34c96e3a3bbb253ec90
Natsuki Iida <natsuki.iida0511@gmail.com>
2024-10-29 18:40:33 +0100

    add responsive design

---
+++ src/components/News/NewsMobile.styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { Colors, Fonts } from "../../global/Global.Styles";
+
+export const Container = styled.div`
+  width: 80%;
+  height: 20rem;
+  margin: 6rem auto;
+  background-color: ${Colors.SecondaryOpacity};
+  display: flex;
+  position: relative;
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  height: 80%;
+  width: 80%;
+  left: 10%;
+  top: 7%;
+`;
+
+export const Title = styled.div`
+  font-size: 1.5rem;
+  font-family: ${Fonts.CAVEAT};
+  `;
+
+  export const Description = styled.div`
+  font-family: ${Fonts.NOTO_SANS_JP};
+  font-size: 1rem;
+  padding: 1.5rem 0.5rem;
+  `;
---
+++ src/components/News/NewsMobile.tsx
@@ -0,0 +1,16 @@
+import * as S from "../../components/News/NewsMobile.styles"
+
+const NewsMobile = () => {
+return (
+    <S.Container>
+        <S.Content>
+            <S.Title>News:</S.Title>
+            <S.Description>
+                デコパージュ教室準備中（2024年11月開講予定）
+            </S.Description>
+        </S.Content>
+    </S.Container>
+)
+}
+
+export default NewsMobile;
