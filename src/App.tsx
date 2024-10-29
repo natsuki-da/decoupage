66b40285506ef5c33d8c40168a53f4e55e0c6359
Natsuki Iida <natsuki.iida0511@gmail.com>
2024-10-29 18:16:24 +0100

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Paths } from "./types/enums";
 const Home = lazy(() => import("./components/Home/Home"));
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path={Paths.HOME} element={<Home />}/>
-//       </Routes>
-//     </Router>
-//   )
-// }
-
 function App() {
   return (
     <Router>
