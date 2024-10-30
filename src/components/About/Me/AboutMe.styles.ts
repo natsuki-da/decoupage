585bf4b3d40fc3f5b45843fefe59cb6a7d0273d7
Natsuki Iida <natsuki.iida0511@gmail.com>
2024-10-30 09:09:04 +0100

    minor changes

--- src/components/About/Me/AboutMe.styles.ts
+++ src/components/About/Me/AboutMe.styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Colors, Fonts } from "../../global/Global.Styles";
+import { Colors, Fonts } from "../../../global/Global.Styles";
 
 export const Container = styled.div`
 width: 100%;
