70c900670e4174bc78c3cafbe60e8872f0118c76
Natsuki Iida <natsuki.iida0511@gmail.com>
2024-10-31 18:51:04 +0100

    add responsive design

---
+++ src/components/About/Me/AboutMeMobile.styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { Colors, Fonts } from "../../../global/Global.Styles";
+
+export const Container = styled.div`
+width: 100%;
+background-color: ${Colors.SecondaryOpacity};
+// margin: 6rem 0;
+display: flex;
+`;
+
+export const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem 1rem;
+`;
+
+export const Title = styled.div`
+    font-size: 1rem;
+    font-family: ${Fonts.CAVEAT};
+    margin: 0 0 1rem 0;
+`;
+
+export const Description = styled.div`
+    font-size: 0.5rem;
+    font-family: ${Fonts.NOTO_SANS_JP};
+`;
+
+export const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  padding: 0 1rem 0 0;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+`;
---
+++ src/components/About/School/AboutSchoolMobile.tsx
@@ -0,0 +1,17 @@
+import * as S from "./AboutSchoolMobile.styles"
+
+const AboutSchoolMobile = () => {
+    return (
+        <S.Container>
+            <S.Left>
+                <S.Image src="../../images/welcome.jpeg" alt="welcome-plate" className="welcome" />
+            </S.Left>
+            <S.Right>
+                <S.Title>About School</S.Title>
+                <S.Description>デコパージュサロンについて…</S.Description>
+            </S.Right>
+        </S.Container>
+    )
+}
+
+export default AboutSchoolMobile;
