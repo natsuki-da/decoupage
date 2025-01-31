226ec46314068d1027d671eb32654cef0bfee773
Natsuki Iida <natsuki.iida0511@gmail.com>
2025-01-31 16:06:55 +0100

    go back to the original vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/'
-// })
-
+//https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        lessons: 'Course.tsx/index.html',
-        about_atelier: 'Atelier.tsx/index.html',
-      },
-    },
-  },
-});
+  plugins: [react()],
+  base: '/'
+})
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: 'index.html',
+//         lessons: 'Course.tsx/index.html',
+//         about_atelier: 'Atelier.tsx/index.html',
+//       },
+//     },
+//   },
+// });
 
