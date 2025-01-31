c46546f9341d96f7afa724c7e63f930785c93be3
Natsuki Iida <natsuki.iida0511@gmail.com>
2025-01-31 16:07:08 +0100

    change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { lazy } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Paths } from "./types/enums";
 const Home = lazy(() => import("./components/Home/Home"));
 const Atelier = lazy(() => import("./components/Mobile/Atelier"));
